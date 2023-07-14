1665b34842aae859992b308d66d5e395a636f364
Tomasz Rudnicki <tomek198821@wp.pl>
2023-07-14 14:31:34 +0100

    topScorers - noCors

--- src/components/SerieAMenu/TopScorers.js
+++ src/components/SerieAMenu/TopScorers.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles((theme)=>({
        setLoading(true)
        const fetchTable = async () =>{
    const response = await fetch("https://api.football-data.org/v4/competitions/SA/scorers", {
-	
+	mode:'no-cors',
 	"headers": {
 		"X-Auth-Token": "3cb5b9dd8f50443e97d7c53804bd5634"
 	}
