9e45bed2addf7f9b4f1a6942144bbb45951da161
Tomasz Rudnicki <tomek198821@wp.pl>
2023-07-20 19:51:15 +0100

    fetchTable api file created

---
+++ api/fetchTable.js
@@ -0,0 +1,29 @@
+module.exports = async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://api.football-data.org/v4/competitions/SA/standings?season=2022",
+      {
+        method: "GET",
+        headers: {
+          "X-Auth-Token": "3cb5b9dd8f50443e97d7c53804bd5634",
+        },
+      }
+    );
+    const responseData = await response.json();
+    
+
+    // Process the data if needed
+
+    // Set CORS headers
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://inter-website.vercel.app"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS"); // Adjust this based on your needs
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.json(responseData);
+  } catch (error) {
+    console.error("Error fetching fixtures:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- src/components/SerieAMenu/Table.js
+++ src/components/SerieAMenu/Table.js
@@ -49,16 +49,7 @@ const useStyles = makeStyles((theme)=>({
    useEffect(()=>{
        setLoading(true)
        const fetchTable = async () =>{
-   const response = await fetch(
-     "https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/competitions/SA/standings?season=2022",
-     {
-       method: "GET",
-       headers: {
-         "X-Auth-Token": "3cb5b9dd8f50443e97d7c53804bd5634",
-         Origin: "https://api.football-data.org/v4/competitions/SA/standings",
-       },
-     }
-   );   
+   const response = await fetch("/api/fetchTable");   
      const responseData = await response.json()
      
      const standings = responseData.standings;
