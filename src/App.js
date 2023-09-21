f36ef7ae9223b089032ce719c8d01ee454aa89a5
Tomasz Rudnicki <tomek198821@wp.pl>
2023-09-21 11:00:34 +0100

    removing seperate news components and replacing it with one SingleNews

--- src/App.js
+++ src/App.js
@@ -18,19 +18,12 @@ import ItalianCup from './components/CupsMenu/ItalianCup';
 import Anthem from './components/FansMenu/Anthem';
 import FanToken from './components/FansMenu/FanToken';
 import MainNews from './components/newsFolder/MainNews';
-import NewsOne from './components/newsFolder/NewsOne';
-import NewsTwo from './components/newsFolder/NewsTwo';
-import NewsThree from './components/newsFolder/NewsThree';
-import NewsFour from './components/newsFolder/NewsFour';
-import NewsFive from './components/newsFolder/NewsFive';
-import NewsSix from './components/newsFolder/NewsSix';
-import NewsSeven from './components/newsFolder/NewsSeven';
-import NewsEight from './components/newsFolder/NewsEight';
 import { useState, useContext } from 'react';
 import zIndex from '@material-ui/core/styles/zIndex';
 import { makeStyles } from '@material-ui/core';
 import ProfileForm from './components/ProfileForm';
 import AuthContext, { AuthContextProvider } from './store/auth-context';
+import SingleNews from './components/newsFolder/SingleNews';
 
 
 const useStyles = makeStyles((theme)=>({
@@ -50,26 +43,19 @@ function App() {
           {authCtx.isLoggedIn && (
             <Route path="/profile" element={<ProfileForm />} />
           )}
-          <Route path="/first-team" element={<FirstTeam/>} />
+          <Route path="/first-team" element={<FirstTeam />} />
           <Route path="/staff" element={<Staff />} />
-          <Route path="/coach" element={<Coach/>} />
-          <Route path="/transfers" element={<Transfers/>} />
-          <Route path="/fixtures" element={<Fixtures/>} />
-          <Route path="/table" element={<Table/>} />
-          <Route path="/top-scorers" element={<TopScorers/>} />
-          <Route path="/champions-league" element={<ChampionsLeague/>} />
-          <Route path="/italian-cup" element={<ItalianCup/>} />
-          <Route path="/anthem" element={<Anthem/>} />
-          <Route path="/fantoken" element={<FanToken/>} />
-          <Route path="/mainNews" element={<MainNews/>} />
-          <Route path="/id1" element={<NewsOne/>} />
-          <Route path="/id2" element={<NewsTwo/>} />
-          <Route path="/id3" element={<NewsThree/>} />
-          <Route path="/id4" element={<NewsFour/>} />
-          <Route path="/id5" element={<NewsFive/>} />
-          <Route path="/id6" element={<NewsSix/>} />
-          <Route path="/id7" element={<NewsSeven/>} />
-          <Route path="/id8" element={<NewsEight/>} />
+          <Route path="/coach" element={<Coach />} />
+          <Route path="/transfers" element={<Transfers />} />
+          <Route path="/fixtures" element={<Fixtures />} />
+          <Route path="/table" element={<Table />} />
+          <Route path="/top-scorers" element={<TopScorers />} />
+          <Route path="/champions-league" element={<ChampionsLeague />} />
+          <Route path="/italian-cup" element={<ItalianCup />} />
+          <Route path="/anthem" element={<Anthem />} />
+          <Route path="/fantoken" element={<FanToken />} />
+          <Route path="/mainNews" element={<MainNews />} />
+          <Route path="/:id" element={<SingleNews />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/newsFolder/SingleNews.js
@@ -0,0 +1,102 @@
+import { React, useEffect,useState } from 'react';
+import { Route, useParams } from 'react-router-dom'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme)=>({
+    mainCard:{
+        maxWidth:'1000px'
+    },
+    mainContainer:{
+        marginTop:'80px',
+        justifyContent:'center',
+        display:'flex',
+        [theme.breakpoints.down('sm')]:{
+          marginTop:'70px'
+      }
+    },
+    image:{
+        objectFit:'cover',
+        objectPosition:'top'
+    }
+}))
+   
+export default function SingleNews() {
+        const {id} = useParams()
+    const classes = useStyles()
+    
+    const [singleNews, setSingleNews] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchNewsById = async (id) => {
+            setLoading(true);
+            const response = await fetch(
+              `https://inter-website-default-rtdb.firebaseio.com/items/${id}.json`
+            );
+            const responseData = await response.json();
+
+            if (responseData) {
+                const loadedNews = {
+                  id: id,
+                  title: responseData.title,
+                  picture: responseData.picture,
+                  date: responseData.date,
+                  alt: responseData.alt,
+                  text1: responseData.text1,
+                  text2: responseData.text2,
+                  text3: responseData.text3,
+                };
+                setSingleNews(loadedNews);
+            } else {
+                console.error('Item with ID ${id} not found');
+                setSingleNews(null)
+            }
+            setLoading(false);
+        }
+
+        const itemId = id;
+        fetchNewsById(itemId)
+    }, [])
+    console.log(singleNews)
+  return (
+    <Container className={classes.mainContainer}>
+      <Card className={classes.mainCard}>
+        <Typography
+          gutterBottom
+          variant="h4"
+          component="div"
+          style={{ marginTop: "10px", fontWeight: "bold" }}
+        >
+          {singleNews.title}
+        </Typography>
+        <CardMedia
+          className={classes.image}
+          component="img"
+          height="640"
+          image={singleNews.picture}
+          alt={singleNews.alt}
+        />
+        <CardContent>
+          <Typography variant="body1" style={{ marginBottom: "20px" }}>
+            {singleNews.text1}
+          </Typography>
+          <Typography style={{ marginBottom: "20px" }}>
+            {" "}
+            {singleNews.text2}
+          </Typography>
+          <Typography style={{ marginBottom: "10px" }}>
+            {" "}
+            {singleNews.text3}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+}
