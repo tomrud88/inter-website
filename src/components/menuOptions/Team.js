19b678ddd77c54677c287c2ffb342d7c2760faad
Tomasz Rudnicki <tomek198821@wp.pl>
2023-10-15 10:34:42 +0100

    adding tests to Team component

--- src/components/menuOptions/Team.js
+++ src/components/menuOptions/Team.js
@@ -17,52 +17,64 @@ function Team() {
     const open = Boolean(anchor)
 
     return (
-        <div>
-               <Typography
-                 onClick={handleMenuOpen}
-                 style={{fontWeight:'bold',fontSize:'17px',color:'white'}}
-                  >TEAM
-                </Typography>
-                <Popper
-                style={{zIndex:2}}
-                open={open}
-                anchorEl={anchor}
-                placement='bottom-start'
-                transition
-                disablePortal
-                >
-                <Paper >
-                <ClickAwayListener onClickAway={handleClose}>
-                <MenuList
-                variant='selectedMenu'
-                id='fade-menu'
-                aria-labelledby= 'fade-button'
+      <div>
+        <Typography
+          onClick={handleMenuOpen}
+          style={{ fontWeight: "bold", fontSize: "17px", color: "white" }}
+          data-testid="team-typography"
+        >
+          TEAM
+        </Typography>
+
+        <Popper
+          style={{ zIndex: 2 }}
+          open={open}
+          anchorEl={anchor}
+          onClose={handleClose}
+          placement="bottom-start"
+          transition
+          disablePortal
+          data-testid="team-popper"
+        >
+          <Paper>
+            <ClickAwayListener onClickAway={handleClose}>
+              <MenuList
+                variant="selectedMenu"
+                id="fade-menu"
+                aria-labelledby="fade-button"
                 open={open}
                 anchorEl={anchor}
                 onClose={handleClose}
-                TransitionComponent={Fade}
-                disableRestoreFocus
-                >
-            
-            <MenuItem onClick={handleClose}> 
-            <Link href="/first-team" underline='none'>FIRST TEAM</Link>
-             </MenuItem >
-                    <MenuItem onClick={handleClose}>
-                    <Link href="/staff"
-                    underline='none'>STAFF</Link>
-                     </MenuItem >
-                    <MenuItem onClick={handleClose}
-                     ><Link href="/coach"
-                     underline='none'>COACH</Link></MenuItem >
-                    <MenuItem onClick={handleClose}
-                   ><Link href="/transfers"
-                   underline='none'>TRANSFERS</Link></MenuItem >
-                </MenuList>
-                </ClickAwayListener>
-                </Paper>
-                </Popper>
-                </div>
-    )
+                transitioncomponent={Fade}
+                disablerestorefocus
+                data-testid="team-menu-list"
+              >
+                <MenuItem data-testid="team-menu-item" onClick={handleClose}>
+                  <Link href="/first-team" underline="none">
+                    FIRST TEAM
+                  </Link>
+                </MenuItem>
+                <MenuItem data-testid="team-menu-item" onClick={handleClose}>
+                  <Link href="/staff" underline="none">
+                    STAFF
+                  </Link>
+                </MenuItem>
+                <MenuItem data-testid="team-menu-item" onClick={handleClose}>
+                  <Link href="/coach" underline="none">
+                    COACH
+                  </Link>
+                </MenuItem>
+                <MenuItem data-testid="team-menu-item" onClick={handleClose}>
+                  <Link href="/transfers" underline="none">
+                    TRANSFERS
+                  </Link>
+                </MenuItem>
+              </MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Popper>
+      </div>
+    );
 }
 
 export default Team                
---
+++ src/components/menuOptions/Team.test.js
@@ -0,0 +1,30 @@
+import { render,fireEvent, queryAllByTestId } from "@testing-library/react";
+import Team from "./Team";
+
+describe("Team component", () => {
+  test("clicking on the typography should open the menu", () => {
+    const { getByTestId,queryByTestId } = render(<Team />);
+    const typography = getByTestId("team-typography");
+    expect(queryByTestId("team-popper")).toBeNull();
+    fireEvent.click(typography);
+    expect(queryByTestId("team-popper")).not.toBeNull();
+  });
+    
+    test("clicking on a menu item should close the menu", () => {
+      const { getByTestId, queryByTestId, queryAllByTestId } = render(<Team />);
+        const typography = getByTestId("team-typography");
+      expect(queryByTestId("team-popper")).toBeNull();
+      fireEvent.click(typography);
+      expect(queryByTestId("team-popper")).not.toBeNull();
+      
+      const items = queryAllByTestId('team-menu-item'); 
+      
+      items.forEach((item) => {
+        fireEvent.click(item);
+        expect(queryByTestId("team-popper")).toBeNull();
+      })
+       
+    });
+
+  
+});
